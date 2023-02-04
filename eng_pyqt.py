89690fd46ee01bce2e8219a926786a57d15dac0e
rin8351 <57259194+rin8351@users.noreply.github.com>
2023-02-04 22:35:20 +0400

    Update eng_pyqt.py

--- eng_pyqt.py
+++ eng_pyqt.py
@@ -11,7 +11,6 @@ class Language(QMainWindow):
         super().__init__()
         self.windowss = QFrame()
         self.shet = 0
-        self.sh2 = 0
         self.stat = dict()
         self.shet_know = 0
         self.shet_prodol_or_snulya=0 # продолжить незаконченный тест в прошлый раз или весь список проходить
@@ -21,6 +20,7 @@ class Language(QMainWindow):
         self.sp = ['Англ', 'Русс']
         self.vib=[]
         self.check_value =False
+        self.test_is_filled = False
         self.data_from_xls()
         self.main()
 
@@ -82,6 +82,7 @@ class Language(QMainWindow):
             json.dump(self.stat, fle, indent='    ', ensure_ascii=False)
 
     def main(self):
+        self.sh2 = 0
         self.frame_main = QFrame()
         self.frame_up = QFrame()
         font = QFont("Times", 10)
@@ -106,13 +107,22 @@ class Language(QMainWindow):
         self.check_r_or_st = QCheckBox('Показывать слова рандомно')
         self.choose_lang = QLabel('Выберите язык')
         self.choose_lang .setFont(font)
-        self.sp = ['Англ', 'Русс']
         self.radios = []
         for i in self.sp:
             rb = QRadioButton(i)
             self.radios.append(rb)
-        self.btn3 = QPushButton("Начать")
-
+        self.btn3 = QPushButton("Начать тест")
+        self.btn_witn_zero = QPushButton("Пройти тест с нуля")
+        self.but_contin = QPushButton("Продолжить\n сохраненный тест")
+        self.clear_save = QPushButton("Очистить\n сохраненный тест")
+        self.clear_save.setVisible(False)
+        self.but_contin.setVisible(False)
+        self.btn_witn_zero.setVisible(False)
+        with open('sohranen.txt', 'r') as f:
+            if f.read():
+                self.but_contin.setVisible(True)
+                self.clear_save.setVisible(True)
+                self.btn3.setText('Начать тест с нуля')
         layout_frame_up = QVBoxLayout()
         layout_frame_up.addWidget(self.lb_start)
         layout_frame_up.addWidget(self.ent_less)
@@ -128,6 +138,9 @@ class Language(QMainWindow):
             layout_frame_down.addWidget(rb)
         layout_frame_down.addWidget(self.lb_err)
         layout_frame_down.addWidget(self.btn3)
+        layout_frame_down.addWidget(self.btn_witn_zero)
+        layout_frame_down.addWidget(self.but_contin)
+        layout_frame_down.addWidget(self.clear_save)
         self.frame_down.setLayout(layout_frame_down)
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.frame_up)
@@ -135,6 +148,36 @@ class Language(QMainWindow):
         self.frame_main.setLayout(main_layout)
         self.setCentralWidget(self.frame_main)
         self.btn3.clicked.connect(self.checks)
+        self.but_contin.clicked.connect(self.continue_funk)
+        self.clear_save.clicked.connect(self.ochist_file)
+        self.btn_witn_zero.clicked.connect(self.checks_zero)
+
+    def checks_zero(self):
+        self.data_from_xls()
+        self.over()
+        self.sh2 = 0
+        self.checks()
+
+    def continue_funk(self):
+        self.shet_prodol_or_snulya +=1
+        with open('sohranen.txt',encoding='utf-8') as f:
+            data = f.read()
+        self.test_from_file = ast.literal_eval(data)
+        places2 = []
+        with open('num_less.txt', 'r') as filehandle:
+            for line in filehandle:
+                currentPlace = line[:-1]
+                places2.append(currentPlace)
+        places3 = []
+        with open('for_sohr.txt', 'r') as filehandle:
+            for line in filehandle:
+                currentPlace = line[:-1]
+                places3.append(currentPlace)
+        self.vib=places3[0]
+        self.for_table_sp=places3[1].split(",")  
+        self.checks() 
+        self.com_vib_dict()
+        self.testting()
 
     def checks(self):
         self.check_value = self.check_r_or_st.isChecked()
@@ -181,11 +224,11 @@ class Language(QMainWindow):
         layout_frame_up.addWidget(self.btn_save)
         self.frame_up2.setLayout(layout_frame_up)
 
-        self.btn_continue = QPushButton("Продолжить")
+        self.btn_continue = QPushButton("Начать тест")
         self.btn_continue.setFixedHeight(40)
         self.btn_continue.setFixedWidth(100)
-        self.label_stat = QLabel("")
-        self.label_total = QLabel("")
+        self.label_stat = QLabel("Статистика слова")
+        self.label_total = QLabel("Всего слов")
         font = QFont("Times", 16)
         self.label_question = QLabel("")  # Слово
         self.label_question.setFont(font)
@@ -205,10 +248,20 @@ class Language(QMainWindow):
         layout_frame_know.addWidget(self.but_know)
         self.frame_know.setLayout(layout_frame_know)
 
-        layout_frame_repeat = QVBoxLayout()
+        layout_frame_repeat = QVBoxLayout()  # Блок для повтора, статистики вернух слов и кнопки "Повторить"
         self.label_end = QLabel("")
-        self.label_end.setFont(font)
+        font3 = QFont("Times", 14)
+        self.label_end.setFont(font3)
+        self.label_count_right = QLabel("")
+        font2 = QFont("Times", 10)
+        self.label_count_right.setFont(font2)
+        self.again_but = QPushButton("Повторить")
+        self.again_label = QLabel("")
+        self.again_label.setFont(font2)
         layout_frame_repeat.addWidget(self.label_end)
+        layout_frame_repeat.addWidget(self.label_count_right)
+        layout_frame_repeat.addWidget(self.again_but)
+        layout_frame_repeat.addWidget(self.again_label)
         self.frame_repeat.setLayout(layout_frame_repeat)
 
         main_layout = QVBoxLayout()
@@ -221,11 +274,33 @@ class Language(QMainWindow):
         self.btn_back.clicked.connect(self.back)
         self.btn_continue.clicked.connect(self.testting)
         self.but_know.clicked.connect(self.know)
+        self.again_but.clicked.connect(self.again)
+        self.btn_save.clicked.connect(self.sohran)
+
+    def sohran(self):
+        with open('sohranen.txt', 'w+', encoding='utf-8') as file:
+            json.dump(self.test, file, indent='    ', ensure_ascii=False)
+        if self.vib == 'Русс':
+            otvet='Англ'
+        else:
+            otvet='Русс'
+        places = [self.vib,otvet]
+        with open('for_sohr.txt', 'w') as filehandle:  
+            for listitem in places:
+                filehandle.write('%s\n' % listitem)
+        a=[self.st,self.en,'Words']
+        with open('num_less.txt', 'w') as filehandle:  
+            for listitem in a:
+                filehandle.write('%s\n' % listitem)
+
+    def again(self):
+        self.shet_for_povtor=1
+        self.testting()
 
     def know(self):
         self.but_know.setStyleSheet('QPushButton {background-color: lime; color: white;}')
         self.count_right += 1
-        self.shet_know = 0
+        self.shet_know = 1
 
         for i in self.stat['Words']:
             if self.r in self.stat['Words'][i]:
@@ -255,6 +330,8 @@ class Language(QMainWindow):
         f.close()
         file=open('for_sohr.txt','w')
         file.close()
+        self.frame_main.deleteLater()
+        self.main()
 
     def prodoljit(self):
         self.shet_prodol_or_snulya +=1
@@ -274,7 +351,7 @@ class Language(QMainWindow):
         self.vib=places3[0]
         self.for_table_sp=places3[1].split(",")
         self.com_vib_dict()
-        self.testting()
+        self.menu2()
 
     def testting(self):
         if self.shet_prodol_or_snulya==0:
@@ -311,20 +388,19 @@ class Language(QMainWindow):
                     if i[self.vib] not in self.test_count:
                         self.test_count.append(i[self.vib])
             self.test = test2
+        self.test_is_filled = True
         if self.test != [] and self.stat_min_score != {}:  # Продолжение теста, каждое нажатие кнопки "Next"
-            self.label_stat.setText("Статистика слова")
-            self.label_total.setText("Всего слов")
+            self.btn_continue.setText('Дальше')
+            self.again_label.setText('')
+            self.label_count_right.setText('')
             self.but_know.setVisible(True)
+            self.again_but.setVisible(False)
             self.label_end.setText('')
             if self.sh2 % 2 == 0: # Четное нажатие клавиши, показывает вопрос
+                self.shet_know=0
                 self.but_know.setStyleSheet('QPushButton {background-color: red; color: white;}')
                 self.but_know.setDisabled(False)
                 self.label_answer.setText('')  
-                if self.shet_know==1:
-                    if self.slovo_dlya_povtora_testa!=0:
-                        self.bez_otvet.append(self.slovo_dlya_povtora_testa)
-
-                self.shet_know = 0
                 self.for_table_main = dict()
                 if self.check_value==True:
                     self.rand_num = random.choice(list(self.stat_min_score.keys()))
@@ -381,8 +457,6 @@ class Language(QMainWindow):
             else:   # Нечетное нажатие клавиши прохождения теста, открытие результата
                 self.but_know.setDisabled(True)
                 self.count_all += 1
-                self.shet_know = 1  # счетчик, нажата ли кнопка Know
-
                 self.sh2 += 1
                 for i in self.ts:
                     for j in i:
@@ -396,35 +470,51 @@ class Language(QMainWindow):
                         self.stat_min_score.pop(nm)
                         if i[self.vib] in self.test_count:
                             self.test_count.remove(i[self.vib])
+                if self.shet_know==0:
+                    if self.slovo_dlya_povtora_testa!=0:
+                        self.bez_otvet.append(self.slovo_dlya_povtora_testa)
         else:
+            self.label_count_right.setText('')
             self.but_know.setVisible(False)
-            if self.shet_know==1:
+            if self.shet_know==0:
                 if self.slovo_dlya_povtora_testa!=0:
                     self.bez_otvet.append(self.slovo_dlya_povtora_testa)
             self.label_end.setText('Список слов закончился,\n начинается заново')
+            self.label_end.setStyleSheet('color: red')
+            self.label_total.setText('')
+            self.label_stat.setText('')
             self.label_answer.setText('')
             self.label_question.setText('')
+            if self.count_all != 0:
+                prots = int(round((100 / self.count_all * self.count_right), 2))
+                self.label_count_right.setText(f'Общее число={self.count_all}, Верно={self.count_right}, процент верных = {prots}.')
+            if self.bez_otvet !=[]:
+                self.again_label.setText('Есть слова без ответа, повторить?')
+                self.again_but.setVisible(True)
             self.data_from_xls()
-            self.sp = ['Англ', 'Русс']
-            self.shet = 0
-            self.ochist_file()   
-            self.alls2 = []
-            for i in self.alls:
-                if i['Lesson'] >= self.st and i['Lesson'] <= self.en:
-                    self.alls2.append(i)
-            self.alls = self.alls2
-            self.slovo_dlya_povtora_testa=0
+            self.over()
+
+    def over(self):
+        self.shet = 0
+        self.alls2 = []
+        for i in self.alls:
+            if i['Lesson'] >= self.st and i['Lesson'] <= self.en:
+                self.alls2.append(i)
+        self.alls = self.alls2
+        self.slovo_dlya_povtora_testa=0
 
     def back(self):
         self.frame_main.deleteLater()
         self.main()
+        if self.test_is_filled==True and len(self.alls) != len(self.test):
+            self.btn_witn_zero.setVisible(True)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     language = Language()
     language.show()
     # set window size
-    language.setFixedSize(400, 500)
+    language.setFixedSize(440, 540)
     # set color
     language.setStyleSheet("background-color: #E6E6FA;")
     sys.exit(app.exec_())
